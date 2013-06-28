d703c5b1b4889e0ac65e127cdcb74e9b52ee7578
ivis <leejh@leeJHui-iMac.local>
2013-06-28 17:35:09 +0900

    Initial Commit

---
+++ BirdAnimation_Sheets/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  BirdAnimation_Sheets
+//
+//  Created by ivis on 13. 6. 28..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ BirdAnimation_Sheets/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  BirdAnimation_Sheets
+//
+//  Created by ivis on 13. 6. 28..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
